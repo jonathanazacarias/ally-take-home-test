a745db7bfbeb92b47998a0dc270ba6e7eac206ad
Jonathan Wiley <jonathan.wiley@gmail.com>
2024-12-16 11:56:14 -0600

    Added apartment unit visits index.

--- app/controllers/apartment_unit_visits_controller.rb
+++ app/controllers/apartment_unit_visits_controller.rb
@@ -1,2 +1,5 @@
 class ApartmentUnitVisitsController < ApplicationController
+  def index
+    @apartment_unit_visits = ApartmentUnitVisit.joins(:apartment).all.order("apartments.name, building_name, unit_number, visited_at desc")
+  end
 end
