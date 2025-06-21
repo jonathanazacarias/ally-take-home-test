36b78ee4d73d7b2bcfdc9c409d033045da22551e
jonathanazacarias <40703079+jonathanazacarias@users.noreply.github.com>
2025-06-21 10:53:34 -0700

    New branch to try various optimizations. Removed all code comments from apartment controller.

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -1,9 +1,7 @@
 class ApartmentsController < ApplicationController
 
-  #create a method for the index action
   def index
 
-    # build a "table" that has a summary of the apartment visits for a particular apartment name
     summary_rows = ApartmentUnitVisit
     .joins(:apartment)
     .select(
@@ -12,33 +10,21 @@ class ApartmentsController < ApplicationController
       "COUNT(*) AS visit_count",
       "MAX(apartment_unit_visits.visited_at) AS most_recent_visit"
     )
-    # need to group visits together by apt id and name 
-    # in order to get correct visit count and most recent visit
+
     .group("apartments.id, apartments.name")
 
-  # use the summarized apartment visits to create a "table"
-  # this is actually and array of hashes  where each hash has apt name, 
-  # visit count, visited units list, most recent visit
-
-  # .map do |row| ... end => for each row in summary_rows, run the block,
-  # and collect the return value into a new array.
   @apartments = summary_rows.map do |row|
 
-    # we need to get the list of visited units for each apartment name
     visited_units = ApartmentUnitVisit
       .where(apartment_id: row.apartment_id)
-      .distinct
-      .pluck(:building_name, :unit_number)
-      # some of the data is missing either a building name or a unit number
+      .distinct 
+      .pluck(:building_name, :unit_number) 
       .map do |building, unit|
         building_str = building.presence || ""
         unit_str = unit.presence || ""
         "#{building_str} - #{unit_str}"
       end
-      # we can just use the built in sort to sort the final unit names
       .sort_by { |name| name.downcase }
-    
-    # actually create the hash
     {
       apartment_name: row.apartment_name,
       visit_count: row.visit_count,
@@ -47,7 +33,7 @@ class ApartmentsController < ApplicationController
     }
   end
 
-    # we want to sort the final table by the apartment name with the most recent visit
     @apartments.sort_by! { |row| row[:most_recent_visit] || Time.at(0) }.reverse!
+
   end
 end
