90b0bdf32664e3eac782e107cfe9518fc770b76e
jonathanazacarias <40703079+jonathanazacarias@users.noreply.github.com>
2025-06-25 12:34:11 -0700

    Optomize apartments controller for time complexity.

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -12,33 +12,39 @@ class ApartmentsController < ApplicationController
     )
     .group("apartments.id, apartments.name")
 
-    all_unit_visits = ApartmentUnitVisit
+    all_unique_unit_visits = ApartmentUnitVisit
     .select(:apartment_id, :building_name, :unit_number)
     .distinct
     
-    visits_by_apartment = all_unit_visits.group_by { |visit| visit.apartment_id }
+    unit_names_by_apartment = all_unique_unit_visits.map do |visit|
+      building = visit.building_name.presence
+      unit = visit.unit_number.presence
+
+      unit_name = if building && unit
+                    "#{building} - #{unit}"
+                  elsif building
+                    building
+                  else
+                    unit || "" # default to empty string if both are nil
+                  end
+    
+      [visit.apartment_id, unit_name]
+    end
+    .group_by(&:first) # groups by apartment_id, returns a hash by apt id
+    .transform_values do |pairs|
+      pairs.map(&:last).uniq.sort_by(&:downcase)
+    end
      
-    # O(n*m)
     @apartments = summary_rows.map do |row|
-    raw_units = visits_by_apartment[row.apartment_id] || []
+      {
+        apartment_name: row.apartment_name,
+        visit_count: row.visit_count.to_i,
+        visited_units: unit_names_by_apartment[row.apartment_id] || [], 
+        most_recent_visit: row.most_recent_visit&.to_time
+      }
+    end
 
-    visited_units = raw_units.map do |v|
-      building_str = v.building_name.presence || ""
-      unit_str = v.unit_number.presence || ""
-      "#{building_str} - #{unit_str}"
-
-    end.sort_by { |v| v.downcase }
-
-    {
-      apartment_name: row.apartment_name,
-      visit_count: row.visit_count.to_i,
-      visited_units: visited_units,
-      most_recent_visit: row.most_recent_visit&.to_time
-    }
-  end
-
-  @apartments.sort_by! { |row| row[:most_recent_visit] || Time.at(0) }.reverse!
-
+    @apartments.sort_by! { |row| row[:most_recent_visit] || Time.at(0) }.reverse!
 
   end
 end
